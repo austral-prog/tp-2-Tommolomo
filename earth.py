f5e1ec3d54097ed51ba9da690e1ef8ca65647c64
tfarina <134532408+Tommolomo@users.noreply.github.com>
2024-08-19 09:52:36 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    varx = x<y
+    vary = y<x
+    print(f"The result of {x} comes first in the dictionary than {y} is {varx}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {vary}.")
